dd9abd2311c19daba86aaec975553fdf7730dba5
Sarah G <leihuayi@gmail.com>
2024-07-18 17:08:48 +0200

    Allow all origins

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["localhost", "0.0.0.0", "https://mirai-api.c0.cloud-pi-native.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
