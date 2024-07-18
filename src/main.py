5cb17e8d8bd734ed31cff89aa8e318a117e25da4
Sarah G <leihuayi@gmail.com>
2024-07-18 12:05:21 +0200

    API working similarily to paddlehub

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import base64, io
+from typing import List
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse, FileResponse
@@ -14,6 +15,9 @@ from fastapi import (
 from paddleocr import PaddleOCR
 from PIL import Image
 import numpy as np
+from pydantic import BaseModel
+
+
 
 app = FastAPI()
 
@@ -34,6 +38,9 @@ OCRCustom=PaddleOCR(
     use_angle_cls=False
 )
 
+class ImageRequest(BaseModel):
+    images: List[str]
+
 
 
 @app.get("/", response_class=PlainTextResponse)
@@ -43,15 +50,30 @@ def home():
 
 
 @app.post("/")
-async def ocr(image: str = Body(..., embed=True)):
-    image_data = base64.b64decode(image)
-    buffer = io.BytesIO(image_data)
-    # Open the image using PIL
-    img = Image.open(buffer)
-    # Convert the image to a numpy array
-    img_array = np.array(img)
-    print(img_array.shape)
-    result = OCRCustom.ocr(img_array)
-    for line in result:
-        print(line)
-    return result
+async def ocr(request: ImageRequest):
+    print()
+    try:
+        for encoded_image in request.images:
+            image_data = base64.b64decode(encoded_image)
+            buffer = io.BytesIO(image_data)
+            # Open the image using PIL
+            img = Image.open(buffer)
+            # Convert the image to a numpy array
+            img_array = np.array(img)
+            print(img_array.shape)
+            result = OCRCustom.ocr(img_array)
+
+        json_response = {
+            'msg': 'Success',
+            'results': result,
+            'status': '200'
+        }
+
+
+    except Exception as e:
+        json_response = {
+            'msg': str(e),
+            'results': result,
+            'status': '500'
+        }
+    return json_response
--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import requests
 import base64
+import glob
 
-with open("image_test.jpg", "rb") as image_file:
-    encoded_data = base64.b64encode(image_file.read()).decode()
+for img_path in glob.glob("test_images/*"):
+    with open(img_path, "rb") as image_file:
+        encoded_data = base64.b64encode(image_file.read()).decode()
 
-res = requests.post(url='http://localhost:5000/',
-                    json={"image": encoded_data})
-print(res.json())
+    res = requests.post(
+                        url='http://localhost:5000/',
+                        # url='https://mirai-ocr-api.cloud-pi-native.com/predict/ocr_system',
+                        json={"images": [encoded_data]}).json()['results'][0]
+    print("-------",len(res))
+    print(res)
+    print("\n\n")
