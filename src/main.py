b69cb91345c8316962e0af386d52fe6d48fc64fa
vjourne <journe.victor@gmail.com>
2024-08-14 13:41:14 +0000

    Treat pdf as form data, binary body. Need to answer the converted jpg to the frontend

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import base64
 import io
-from typing import List
+from typing import List, Optional
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse, FileResponse
@@ -19,6 +19,7 @@ import numpy as np
 from pydantic import BaseModel
 import json
 from pathlib import Path
+from pdf2image import convert_from_bytes
 
 app = FastAPI()
 
@@ -54,29 +55,46 @@ def home():
 
 
 @app.post("/")
-async def ocr(request: ImageRequest):
+async def ocr(file: UploadFile = File(...), format: Optional[str] = None):
+    # print("Content type:", file.content_type)
+    print("Filename:", file.filename)
+    ext = Path(file.filename).suffix.lower()
     try:
         formatted_result = []
-        for encoded_image in request.images:
-            print(encoded_image)
-            image_data = base64.b64decode(encoded_image)
-            buffer = io.BytesIO(image_data)
-            # Convert the image to a numpy array
-            img_array = np.array(Image.open(buffer))
-            # FORMAT OF RESULT = List of elements E found on image
-            # where E is a list containing
-            # 1. The bbox coordinates (List of [A,B] where A,B are a corner coordinates)
-            # 2. The text infos (List of [A,B] where A=Text identified and B=confidence)
-            result = OCRCustom.ocr(img_array)
+    
+        # Convert the image to a numpy array
+        if ext in ['.jpg', '.png']:
+            buffer = io.BytesIO(await file.read())
+            img = Image.open(buffer)
+            img_array = np.array(img)
 
-            for element in result[0]:
-                bbox, (text, confidence) = element
-                formatted_result.append({
-                    'confidence': confidence,
-                    'text': text,
-                    'text_region': [[int(x), int(y)] for x, y in bbox]
-                })
+        elif ext == ".pdf":
+            img = convert_from_bytes(await file.read())
+            # new_height = 800
+            # img = img.resize((int(img.width * new_height / img.height), new_height))
+            img_array = np.array(img[0])
+            # TODO: combined all page to y dimension
+        else:
+            json_response = {
+                'msg': 'Failed',
+                'status': '400'
+            }
+            raise HTTPException(status_code=400, detail=json.dumps(json_response))
+        
+        # FORMAT OF RESULT = List of elements E found on image
+        # where E is a list containing
+        # 1. The bbox coordinates (List of [A,B] where A,B are a corner coordinates)
+        # 2. The text infos (List of [A,B] where A=Text identified and B=confidence)
+        # import pdb; pdb.set_trace()
 
+        result = OCRCustom.ocr(img_array)
+        for element in result[0]:
+            bbox, (text, confidence) = element
+            formatted_result.append({
+                'confidence': confidence,
+                'text': text,
+                'text_region': [[int(x), int(y)] for x, y in bbox]
+            })
         json_response = {
             'msg': 'Success',
             'results': [formatted_result],
@@ -90,6 +108,5 @@ async def ocr(request: ImageRequest):
             'status': '500'
         }
         raise HTTPException(status_code=500, detail=json.dumps(json_response))
-
 
     return json_response
--- tests/test_app.py
+++ tests/test_app.py
@@ -15,23 +15,27 @@ def test_home_route():
     assert response.text == "API endpoint for OCR"
 
 def test_ocr_with_valid_image():
+    # import pdb; pdb.set_trace()
     if len(list((test_path.glob("data/valid/*")))) < 1:
         raise FileNotFoundError("You must create a folder test_images and put some images in it to test the API !")
 
     for img_path in test_path.glob("data/valid/*"):
         with open(img_path, "rb") as image_file:
-            encoded_data = base64.b64encode(image_file.read()).decode()
+            files = {"file": (img_path.name, image_file, "multipart/form-data")}
 
-        res = client.post(
-                            url='http://localhost:5000/',
-                            # url='https://ocr.c99.cloud-pi-native.com/predict/ocr_system',
-                            json={"images": [encoded_data]},
-                            headers={'Authorization': 'Basic dXNlcl9vY3I6SlFWRDRxNThzMg=='}).json()
-        print("-------", res['msg'])
-        print(res['results'])
-        print("\n\n")
-        # import pdb; pdb.set_trace()
-        assert len(res['results']) > 0
+            #encoded_data = base64.b64encode(image_file.read()).decode()
+
+            res = client.post(
+                                url='http://localhost:5000/',
+                                # url='https://ocr.c99.cloud-pi-native.com/predict/ocr_system',
+                                files=files,
+                                # json={"images": [encoded_data]},
+                                headers={'Authorization': 'Basic dXNlcl9vY3I6SlFWRDRxNThzMg=='}).json()
+            print("-------", res['msg'])
+            #print(res['results'])
+            print("\n\n")
+            # import pdb; pdb.set_trace()
+            assert len(res['results']) > 0
 
 def test_ocr_with_invalid_image():
     request_data = {"images": ["invalid_base64_string"]}
