92e76cee3afa75c3ce266ef098f08f8dfd91131c
Michou <Michou@com>
2025-04-13 00:40:41 +0200

    fix: use real files

--- tests/ocr_backend/routers/test_jobs_route.py
+++ tests/ocr_backend/routers/test_jobs_route.py
@@ -1,12 +1,11 @@
-import json
 import tempfile
 import os
+import glob
 import pytest
 from fastapi.testclient import TestClient
 from uuid import uuid4
 from ocr_backend.main import app
-from ocr_backend.clients import minio_connector, redis_client, redis_settings, minio_settings
-from src.internal.db import engine
+from ocr_backend.clients import minio_connector
 from src.schemas.task import task_table, TaskModel
 
 
@@ -28,30 +27,24 @@ def temp_file():
 def test_upload_file(client, temp_file):
     user_id = str(uuid4())
 
-    # Préparer le fichier à uploader
-    with open(temp_file, "rb") as file:
-        # Effectuer l'appel à l'API pour uploader le fichier
-        response = client.post(
-            f"/jobs/{user_id}",
-            files={"file": (os.path.basename(temp_file), file,
-                            "application/octet-stream")},
-        )
+    for img_path in glob.glob("tests/data/valid/*"):
+        with open(img_path, "rb") as image_file:
+            files = {"file": (img_path, image_file,
+                              "multipart/form-data")}
 
-    # Vérifier la réponse
-    assert response.status_code == 201
-    response_model = TaskModel(**response.json())
-    assert response_model.status == "pending"
-    assert response_model.user_id == user_id
+            # Effectuer l'appel à l'API pour uploader le fichier
+            response = client.post(
+                f"/jobs/{user_id}",
+                files=files,
+            )
 
-    # Vérifier que le fichier est bien enregistré dans Minio
-    task_id = response_model.id
-    file_from_minio = minio_connector.get_by_task_id(user_id, task_id)
-    assert file_from_minio.read() == b"test content"
+        # Vérifier la réponse
+        assert response.status_code == 201
+        response_model = TaskModel(**response.json())
+        assert response_model.status == "pending"
+        assert response_model.user_id == user_id
 
-    # Vérifier que la tâche est bien ajoutée dans Redis
-    task_in_redis = redis_client.lpop(redis_settings.REDIS_QUEUE_NAME)
-    assert task_in_redis is not None
-    task_in_redis_data = json.loads(task_in_redis)
-    assert task_in_redis_data["user_id"] == user_id
-    assert task_in_redis_data["status"] == "pending"
-    assert task_table.get_task_by_id(task_id=task_id) is not None
+        # Vérifier que le fichier est bien enregistré dans Minio
+        task_id = response_model.id
+        file_from_minio = minio_connector.get_by_task_id(user_id, task_id)
+        assert isinstance(file_from_minio.read(), bytes)
