8a6ab5547d69b914566c6e575b408a2995fc8d23
Michou <Michou@com>
2025-04-12 20:08:43 +0200

    fix: use file instead of memory

--- src/internal/db.py
+++ src/internal/db.py
@@ -4,7 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///:memory:")
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "DATABASE_URL", "sqlite:///./example.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
