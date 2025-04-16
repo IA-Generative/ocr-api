5e67be857da1e217c5ab53c8c011f16217456ed1
Michou <Michou@com>
2025-04-16 08:11:50 +0200

    del: delete deprecated routes

--- ocr_backend/main.py
+++ ocr_backend/main.py
@@ -4,7 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from .routers.task import router as task_router
 from .routers.health import router as health_router
 from .routers.jobs import router as job_router
-from .routers.deprecated import router as deprecated_router
 from src import __name__, __version__
 
 app = FastAPI(title=__name__, version=__version__)
@@ -20,4 +19,3 @@ app.add_middleware(
 app.include_router(task_router)
 app.include_router(health_router)
 app.include_router(job_router)
-app.include_router(deprecated_router)
