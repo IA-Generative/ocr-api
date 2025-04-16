251288995689102d66ecbf8f530c507d2a407907
Michou <Michou@com>
2025-04-16 08:01:34 +0200

    feat: use surya ocr

--- ocr_service/main.py
+++ ocr_service/main.py
@@ -7,9 +7,11 @@ from pdf2image import convert_from_bytes
 import redis
 import minio
 
-from ocr_service.models.paddle import perform_ocr_async
 from src import __name__ as name, __version__
 from ocr_service.utils.image import image_to_base64
+from ocr_service.models.surya_ocr import SuryaOCR
+from ocr_service.configs.surya import SuryaSetting
+
 from src.config.redis import RedisSettings
 from src.config.minio import MinioSettings
 
@@ -18,27 +20,35 @@ from src.schemas.task import task_table, TaskModel, TaskUpdateForm, TaskStatus
 from src.logger import logger
 
 
-if __name__ == "__main__":
-    minio_settings = MinioSettings()
-    minio_client = minio.Minio(
-        endpoint=minio_settings.MINIO_END_POINT,
-        access_key=minio_settings.MINIO_ACCESS_KEY,
-        secret_key=minio_settings.MINIO_SECRET_KEY,
-        secure=False,
-    )
-    minio_connector = MinioConnector(
-        minio_client=minio_client, bucket_name=minio_settings.MINIO_BUCKET_NAME
-    )
+minio_settings = MinioSettings()
+minio_client = minio.Minio(
+    endpoint=minio_settings.MINIO_END_POINT,
+    access_key=minio_settings.MINIO_ACCESS_KEY,
+    secret_key=minio_settings.MINIO_SECRET_KEY,
+    secure=False,
+)
+minio_connector = MinioConnector(
+    minio_client=minio_client, bucket_name=minio_settings.MINIO_BUCKET_NAME
+)
 
-    redis_settings = RedisSettings()
-    redis_client = redis.Redis(
-        host=redis_settings.REDIS_HOST, port=redis_settings.REDIS_PORT, db=0
-    )
+redis_settings = RedisSettings()
+redis_client = redis.Redis(
+    host=redis_settings.REDIS_HOST, port=redis_settings.REDIS_PORT, db=0
+)
+ocr_settings = SuryaSetting()
+ocr_model = SuryaOCR(checkpoint_detection=ocr_settings.SURYA_DETECTION_FOLDER,
+                     checkpoint_recognition=ocr_settings.SURYA_RECOGNITION_FOLDER)
+
+
+def get_task() -> TaskModel:
+    _, task_in_redis = redis_client.brpop(redis_settings.REDIS_QUEUE_NAME)
+    return TaskModel.model_validate(json.loads(task_in_redis))
+
+
+if __name__ == "__main__":
     logger.info("Start to consume")
     while True:
-        _, task_in_redis = redis_client.brpop(redis_settings.REDIS_QUEUE_NAME)
-        task = TaskModel.model_validate(json.loads(task_in_redis))
-
+        task = get_task()
         extras = task.extras if task.extras else {}
         extras["service_name"] = name
         extras["version"] = __version__
@@ -78,6 +88,8 @@ if __name__ == "__main__":
             logger.error(f"No content found for task : {task.id}")
             continue
 
+        logger.debug(f'content-type : {content_type}')
+
         if content_type.startswith("image/"):
             pages = [Image.open(content).convert("RGB")]
 
@@ -114,28 +126,26 @@ if __name__ == "__main__":
             if max_height and int(height) > max_height:
                 page = page.resize(
                     (int(width * max_height / height), max_height))
-            if return_image:
+
+        if return_image:
+            for page in pages:
                 base64_images.append(image_to_base64(page))
 
-            if grayscale:
-                page = ImageOps.grayscale(page)
+        if grayscale:
+            for i in range(len(pages)):
+                page[i] = ImageOps.grayscale(page[i])
 
-        for i in range(0, len(pages), batch_size):
+        for i in range(0, len(pages), ocr_settings.SURYA_DETECTION_BATCH_SIZE):
             t_predict = time.time()
-            batch = pages[i: i + batch_size]
+            batch = pages[i: i + ocr_settings.SURYA_DETECTION_BATCH_SIZE]
             for image in batch:
                 logger.debug(f"{np.array(image).shape}")
                 # TODO : Pdf with differents size of page
 
-            # Si une seule image restante, empile avec une nouvelle dimension
-            if len(batch) == 1:
-                images = np.array(batch[0])
-            else:
-                images = np.stack(batch, axis=0)
+            partial_result = ocr_model.batch_predict(images=batch, langs=[['fr']for _ in batch],
+                                                     detection_batch_size=ocr_settings.SURYA_DETECTION_BATCH_SIZE,
+                                                     recognition_batch_size=ocr_settings.SURYA_RECOGNITION_BATCH_SIZE)
 
-            logger.debug(images.shape)
-
-            partial_result = perform_ocr_async(images)
             formatted_result.extend(partial_result)
 
             page_range = f"{i + 1}" if len(
