eecee0e8d6a9887906a5d0e324bf148df17f28d3
Michou <Michou@com>
2025-04-16 08:12:53 +0200

    fix: add unittest for utils

--- ocr_service/utils/image.py
+++ ocr_service/utils/image.py
@@ -1,8 +1,9 @@
+from PIL import Image
 import base64
 import io
 
 
-def image_to_base64(image, format="PNG"):
+def image_to_base64(image: Image.Image, format="PNG"):
     buffered = io.BytesIO()
     image.save(buffered, format=format)
     return base64.b64encode(buffered.getvalue()).decode("utf-8")
