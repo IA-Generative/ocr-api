c8020644392385e3785d817edea914e86a008770
michou <misu.helal-ali@interieur.gouv.fr>
2025-04-23 12:30:59 +0200

    fix: add failure process

--- ocr_service/main.py
+++ ocr_service/main.py
@@ -1,6 +1,9 @@
+import json
+import traceback
+import os
 from src.utils.usage import resource_monitor
 from src.logger import logger
-from src.schemas.task import TaskModel
+from src.schemas.task import TaskModel, task_table, TaskForm, TaskStatus
 from src.connector.minio_connector import MinioConnector
 from src.config.minio import MinioSettings
 from src.config.redis import RedisSettings
@@ -9,7 +12,7 @@ from ocr_service.configs.surya import SuryaSetting
 from ocr_service.models.surya_ocr import SuryaOCR
 from celery import Celery
 import minio
-import json
+
 
 ocr_settings = SuryaSetting()
 minio_settings = MinioSettings()
@@ -25,29 +28,40 @@ minio_connector = MinioConnector(
 
 redis_settings = RedisSettings()
 app = Celery(
-    'worker', broker=f'redis://{redis_settings.REDIS_HOST}:{redis_settings.REDIS_PORT}/')
+    "worker", broker=f"redis://{redis_settings.REDIS_HOST}:{redis_settings.REDIS_PORT}/"
+)
 
-ocr_model: SuryaOCR = None
-process_ocr: OCRWorker = None
-
-
-def load_model():
-    global ocr_model, process_ocr
-    ocr_model = SuryaOCR(checkpoint_detection=ocr_settings.SURYA_DETECTION_FOLDER,
-                         checkpoint_recognition=ocr_settings.SURYA_RECOGNITION_FOLDER)
-    process_ocr = OCRWorker(minio_connector=minio_connector,
-                            ocr_model=ocr_model, settings=ocr_settings)
+ocr_model = SuryaOCR(
+    checkpoint_detection=ocr_settings.SURYA_DETECTION_FOLDER,
+    checkpoint_recognition=ocr_settings.SURYA_RECOGNITION_FOLDER,
+)
+process_ocr = OCRWorker(
+    minio_connector=minio_connector, ocr_model=ocr_model, settings=ocr_settings
+)
 
 
 @app.task(name="worker.tasks.ocr")
-# @resource_monitor(interval_sec=5, label='worker.tasks.ocr')
+@resource_monitor(
+    interval_sec=os.environ.get("MONITOR_RESSOURCE_EVERY", 5), label="worker.tasks.ocr"
+)
 def launch_task(task_info: dict):
-    global ocr_model, process_ocr
-    if process_ocr is None or ocr_model is None:
-        load_model()
     task = TaskModel.model_validate(json.loads(task_info))
-    task = process_ocr.process_task(task=task)
-    return task.model_dump()
+    try:
+        task = process_ocr.process_task(task=task)
+        return task.model_dump()
+    except Exception as e:
+        task.extras = task.extras if task.extras else {}
+        task.extras["error"] = str(e)
+        task.extras["traceback"] = traceback.format_exc()
+        task_table.update_task(
+            task_id=task.id,
+            form_data=TaskForm(
+                user_id=task.user_id,
+                type=task.type,
+                status=TaskStatus.FAILED.value,
+                extras=task.extras,
+            ),
+        )
 
 
 if __name__ == "__main__":
