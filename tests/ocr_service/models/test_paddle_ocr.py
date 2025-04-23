4f94b77e04b776b7c962aace2cbaac7ea04a0e68
michou <misu.helal-ali@interieur.gouv.fr>
2025-04-23 18:21:23 +0200

    fix: rid off ignore folder ignore

---
+++ tests/ocr_service/models/test_paddle_ocr.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from ocr_service.configs.surya import SuryaSetting
+from ocr_service.models.paddle_ocr import PaddleInferOCR
+from src.schemas.prediction import PredictionOCR
+
+
+def test_predict():
+    settings = SuryaSetting()
+    image = Image.open("tests/data/valid/formulaire-cerfa-complete.png")
+
+    obj = PaddleInferOCR(path_model="models/")
+    actuals = obj.batch_predict(images=[image, image])
+
+    assert len(actuals) == 2
+    for actual in actuals:
+        for instance in actual:
+            assert isinstance(instance, PredictionOCR)
