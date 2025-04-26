2f1921f60535397be2bad5aea776614ccb9afada
michou <misu.helal-ali@interieur.gouv.fr>
2025-04-26 11:41:33 +0200

    fix: unittest

--- tests/integrations/test_ocr_prediction.py
+++ tests/integrations/test_ocr_prediction.py
@@ -1,5 +1,6 @@
 import os
 import json
+from typing import Any, Dict
 
 from PIL import Image
 import numpy as np
@@ -8,13 +9,46 @@ import cv2
 from ocr_service.models.paddle_ocr import PaddleInferOCR
 
 
+import math
+
+
+def assert_dict_almost_equal(d1: Dict[Any, Any], d2: Dict[Any, Any], tol: float = 1e-2):
+    assert d1.keys() == d2.keys(), f"Les clés diffèrent : {d1.keys()} != {d2.keys()}"
+
+    for key in d1:
+        v1 = d1[key]
+        v2 = d2[key]
+
+        if isinstance(v1, float) and isinstance(v2, float):
+            assert math.isclose(
+                v1, v2, abs_tol=tol
+            ), f"Différence sur clé '{key}': {v1} != {v2} avec tolérance {tol}"
+
+        elif isinstance(v1, list) and isinstance(v2, list):
+            assert len(v1) == len(
+                v2
+            ), f"Listes de longueur différente pour clé '{key}': {len(v1)} != {len(v2)}"
+            for i, (x, y) in enumerate(zip(v1, v2)):
+                if isinstance(x, float) and isinstance(y, float):
+                    assert math.isclose(
+                        x, y, abs_tol=tol
+                    ), f"Différence dans liste à l'index {i} pour clé '{key}': {x} != {y} avec tolérance {tol}"
+                else:
+                    assert (
+                        x == y
+                    ), f"Différence dans liste à l'index {i} pour clé '{key}': {x} != {y}"
+
+        else:
+            assert v1 == v2, f"Différence sur clé '{key}': {v1} != {v2}"
+
+
 def test_integration_ocr_paddle_prediction():
     model = PaddleInferOCR("models/")
     filename = "tests/data/valid/identite.jpg"
     basename = os.path.basename(filename)
     base_file, _ = os.path.splitext(basename)
     image = Image.open(filename)
-    output_folder = "tests/data/valid/predictions/"
+    output_folder = "tests/data/predictions/valid"
     all_predictions = model.batch_predict([image])
 
     for predictions, image in zip(all_predictions, [image]):
@@ -37,5 +71,7 @@ def test_integration_ocr_paddle_prediction():
                 (0, 0, 255),
                 2,
             )
-        assert expected == [pred.model_dump() for pred in predictions]
+        for expect, actual in zip(expected, predictions):
+            assert_dict_almost_equal(d1=expect, d2=actual.model_dump())
+
         cv2.imwrite(os.path.join(output_folder, f"{basename}"), image)
