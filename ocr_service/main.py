8dc5c7299cd4e3c24a1604a43c70a86afe561021
michou <misu.helal-ali@interieur.gouv.fr>
2025-04-26 14:07:08 +0200

    feat: add s3 connector and client

--- ocr_service/main.py
+++ ocr_service/main.py
@@ -5,54 +5,17 @@ import time
 from src.utils.usage import resource_monitor
 from src.logger import logger
 from src.schemas.task import TaskModel, task_table, TaskForm, TaskStatus
-from src.connector.minio_connector import MinioConnector
-from src.config.minio import MinioSettings
 from src.config.redis import RedisSettings
 from ocr_service.workers.ocr_worker import OCRWorker
-
+from ocr_service.clients import get_ocr_processor
 from celery import Celery
-import minio
 
-AVAILABLE_MODEL = ["paddle", "surya"]
-
-minio_settings = MinioSettings()
-minio_client = minio.Minio(
-    endpoint=minio_settings.MINIO_END_POINT,
-    access_key=minio_settings.MINIO_ACCESS_KEY,
-    secret_key=minio_settings.MINIO_SECRET_KEY,
-    secure=False,
-)
-minio_connector = MinioConnector(
-    minio_client=minio_client, bucket_name=minio_settings.MINIO_BUCKET_NAME
-)
 
 redis_settings = RedisSettings()
 app = Celery(
     "worker", broker=f"redis://{redis_settings.REDIS_HOST}:{redis_settings.REDIS_PORT}/"
 )
-model_name = os.environ.get("MODEL_NAME", "paddle")
-if model_name == "surya":
-    from ocr_service.configs.surya import SuryaSetting
-    from ocr_service.models.surya_ocr import SuryaOCR
-
-    ocr_settings = SuryaSetting()
-    ocr_model = SuryaOCR(
-        checkpoint_detection=ocr_settings.SURYA_DETECTION_FOLDER,
-        checkpoint_recognition=ocr_settings.SURYA_RECOGNITION_FOLDER,
-    )
-
-elif model_name == "paddle":
-    from ocr_service.configs.paddle import PaddleSetting
-    from ocr_service.models.paddle_ocr import PaddleInferOCR
-
-    ocr_settings = PaddleSetting()
-    ocr_model = PaddleInferOCR(path_model=ocr_settings.PADDLE_OCR_BASE_DIR)
-
-else:
-    raise NotImplementedError(f"{model_name} is not available yet ({AVAILABLE_MODEL})")
-process_ocr = OCRWorker(
-    minio_connector=minio_connector, ocr_model=ocr_model, settings=ocr_settings
-)
+process_ocr: OCRWorker = get_ocr_processor()
 
 
 @app.task(name="worker.tasks.ocr")
