3ad4f17ffcca10b10b677c46c13d580eca3357fb
titigmr <thierry.gmr@outlook.fr>
2025-04-30 18:38:59 +0200

    feat: :zap: add tests in containers

--- Makefile
+++ Makefile
@@ -2,8 +2,7 @@ IMAGE_NAME_OCR_BACKEND=ocr-api
 IMAGE_NAME_OCR_SERVICE=ocr-service
 PYTHONPATH=$(PWD)
 
-.PHONY: build test clean install-test install-test-dep linter tests build-images bump-patch donwload-model
-
+.PHONY: build test install-test install-test-dep linter tests build-images bump-patch donwload-model
 
 
 install-test:
@@ -12,8 +11,6 @@ install-test:
 install-test-dep: install-test
 	sudo apt-get update && sudo apt-get install -y poppler-utils
 	uv sync --group test --group ocr-backend --group ocr-service --group ocr-service-paddle
-
-
 
 linter: install-test-dep
 	uv run ruff check .
@@ -24,34 +21,29 @@ bump-patch: install-test
 bump-minor: install-test
 	uv run cz bump --increment minor
 
-up-env:
+up:
 	docker compose up -d
-	@echo "Attente de 5 secondes pour laisser les conteneurs démarrer..."
-	sleep 5
 
-down-env:
+down:
 	docker compose down || true
 
-tests: install-test-dep up-env
-	export PYTHONPATH=$(PWD) && env $(shell grep -v '^#' .env | xargs) uv run pytest --cov=./ocr_backend --cov=./ocr_service --cov=./src --cov-report=term-missing tests/
+tests: up
+	docker exec ocr-api pytest --cov=./ocr_backend --cov=./src --cov-report=term-missing tests/ocr_backend tests/src/
+	docker exec ocr-service pytest --cov=./ocr_service tests/ocr_service
 
 build-ocr-backend:
-	docker build -t $(IMAGE_NAME_OCR_BACKEND) -f Dockerfiles/ocr_backend/Dockerfile .
+	docker compose build ocr_backend
 
 build-ocr-service:
-	docker build -t $(IMAGE_NAME_OCR_SERVICE)-paddle -f Dockerfiles/ocr_service/Dockerfile.paddle .
-	docker build -t $(IMAGE_NAME_OCR_SERVICE)-surya -f Dockerfiles/ocr_service/Dockerfile.surya .
-
+	docker compose build ocr_service
 
 build-images: build-ocr-backend build-ocr-service
 
-clean:
-	rm -rf tmp || true
-	# docker rmi $(IMAGE_NAME_OCR_BACKEND) || true
-	# docker rmi $(IMAGE_NAME_OCR_SERVICE) || true
-	find . -type d -name "__pycache__" -exec rm -rf {} +
-	find . -type f -name "*.pyc" -delete
-	find . -type f -name "*.pyo" -delete
+upgrade-db:
+	docker exec ocr-api alembic upgrade head
+
+upgrade-revision:
+	docker exec ocr-api alembic revision --autogenerate
 
 cluster:
 	kind create cluster --name ocr --config ./kind/config.yaml
--- tests/ocr_service/workers/test_workers_ocr.py
+++ tests/ocr_service/workers/test_workers_ocr.py
@@ -1,26 +1,20 @@
-from PIL import Image
-import pytest
 from unittest.mock import MagicMock
-from ocr_service.workers.ocr_worker import (
-    OCRWorker,
-    EmptyContentException,
-    FileNotSupported,
-)
-from src.schemas.task import TaskModel
-from src.schemas.input import InputForm
-from ocr_service.models.paddle_ocr import PaddleInferOCR
-from ocr_service.configs.paddle import PaddleSetting
-from src.schemas.task import task_table, TaskForm, TaskStatus
 
+import pytest
+from PIL import Image
+
+from ocr_service.configs.paddle import PaddleSetting
+from ocr_service.models.paddle_ocr import PaddleInferOCR
+from ocr_service.workers.ocr_worker import EmptyContentException, FileNotSupported, OCRWorker
+from src.schemas.input import InputForm
+from src.schemas.task import TaskForm, TaskModel, TaskStatus, task_table
 
 settings = PaddleSetting()
 
 
 @pytest.fixture
 def dummy_task() -> TaskModel:
-    return task_table.insert_new_task(
-        user_id="123", form_data=TaskForm(user_id="123", type="ocr", status="created")
-    )
+    return task_table.insert_new_task(user_id="123", form_data=TaskForm(user_id="123", type="ocr", status="created"))
 
 
 @pytest.fixture
@@ -33,27 +27,21 @@ def mock_model() -> PaddleInferOCR:
     return PaddleInferOCR(path_model=settings.PADDLE_OCR_BASE_DIR)
 
 
-def test_get_content_file_success(
-    mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel
-):
+def test_get_content_file_success(mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel):
     expected_content = b"fake-bytes-content"
     mock_minio.get_by_task_id.return_value = expected_content
 
-    worker = OCRWorker(minio_connector=mock_minio, ocr_model=mock_model)
+    worker = OCRWorker(file_connector=mock_minio, ocr_model=mock_model)
     content = worker.get_content_file(task=dummy_task)
 
     assert content == expected_content
-    mock_minio.get_by_task_id.assert_called_once_with(
-        user_id=dummy_task.user_id, task_id=dummy_task.id
-    )
+    mock_minio.get_by_task_id.assert_called_once_with(user_id=dummy_task.user_id, task_id=dummy_task.id)
 
 
-def test_get_content_file_none(
-    mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel
-):
+def test_get_content_file_none(mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel):
     mock_minio.get_by_task_id.return_value = None
 
-    worker = OCRWorker(minio_connector=mock_minio, ocr_model=mock_model)
+    worker = OCRWorker(file_connector=mock_minio, ocr_model=mock_model)
 
     with pytest.raises(EmptyContentException):
         worker.get_content_file(task=dummy_task)
@@ -61,12 +49,10 @@ def test_get_content_file_none(
     mock_minio.get_by_task_id.assert_called_once()
 
 
-def test_get_content_file_exception(
-    mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel
-):
+def test_get_content_file_exception(mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel):
     mock_minio.get_by_task_id.side_effect = Exception("Connection error")
 
-    worker = OCRWorker(minio_connector=mock_minio, ocr_model=mock_model)
+    worker = OCRWorker(file_connector=mock_minio, ocr_model=mock_model)
 
     with pytest.raises(Exception) as exc_info:
         worker.get_content_file(task=dummy_task)
@@ -76,14 +62,12 @@ def test_get_content_file_exception(
 
 
 def test_set_task_extras(mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel):
-    worker = OCRWorker(minio_connector=mock_minio, ocr_model=mock_model)
+    worker = OCRWorker(file_connector=mock_minio, ocr_model=mock_model)
     actual = worker.set_extras(task=dummy_task)
     assert actual.extras is not None
 
 
-def test_transform_content_error_no_content_type(
-    mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel
-):
+def test_transform_content_error_no_content_type(mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel):
     dummy_task.input = InputForm(
         storage_file_path="tests/data/valid/identite.jpg",
         raw_filename="identite.jpg",
@@ -91,7 +75,7 @@ def test_transform_content_error_no_content_type(
         size=123456,
         content_type="vvv/ssjpg",
     )
-    worker = OCRWorker(minio_connector=mock_minio, ocr_model=mock_model)
+    worker = OCRWorker(file_connector=mock_minio, ocr_model=mock_model)
     dummy_task.extras = {}
     with open("tests/data/valid/identite.jpg", "rb") as f:
         expected_content = f.read()
@@ -99,9 +83,7 @@ def test_transform_content_error_no_content_type(
             worker.transform_content(task=dummy_task, content=expected_content)
 
 
-def test_transform_content_content_type_image(
-    mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel
-):
+def test_transform_content_content_type_image(mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel):
     dummy_task.input = InputForm(
         storage_file_path="tests/data/valid/identite.jpg",
         raw_filename="identite.jpg",
@@ -110,15 +92,13 @@ def test_transform_content_content_type_image(
         content_type="image/jpg",  # Uncomment this line to simulate the absence of content_type
     )
 
-    worker = OCRWorker(minio_connector=mock_minio, ocr_model=mock_model)
+    worker = OCRWorker(file_connector=mock_minio, ocr_model=mock_model)
     with open("tests/data/valid/identite.jpg", "rb") as f:
         actual = worker.transform_content(task=dummy_task, content=f)
         assert isinstance(actual[0], Image.Image)
 
 
-def test_transform_content_content_type_pdf(
-    mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel
-):
+def test_transform_content_content_type_pdf(mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel):
     dummy_task.input = InputForm(
         storage_file_path="tests/data/valid/cerfa_13750-05-1.pdf",
         raw_filename="cerfa_13750-05-1.pdf",
@@ -127,7 +107,7 @@ def test_transform_content_content_type_pdf(
         content_type="application/pdf",  # Uncomment this line to simulate the absence of content_type
     )
 
-    worker = OCRWorker(minio_connector=mock_minio, ocr_model=mock_model)
+    worker = OCRWorker(file_connector=mock_minio, ocr_model=mock_model)
 
     with open("tests/data/valid/cerfa_13750-05-1.pdf", "rb") as f:
         actual = worker.transform_content(task=dummy_task, content=f)
@@ -135,9 +115,7 @@ def test_transform_content_content_type_pdf(
         assert len(actual) == 1
 
 
-def test_predict_on_pages(
-    mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel
-):
+def test_predict_on_pages(mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel):
     dummy_task.input = InputForm(
         storage_file_path="tests/data/valid/identite.jpg",
         raw_filename="identite.jpg",
@@ -145,16 +123,14 @@ def test_predict_on_pages(
         size=123456,
         content_type="image/jpg",  # Uncomment this line to simulate the absence of content_type
     )
-    worker = OCRWorker(minio_connector=mock_minio, ocr_model=mock_model)
+    worker = OCRWorker(file_connector=mock_minio, ocr_model=mock_model)
 
     image = Image.open("tests/data/valid/identite.jpg")
     actual = worker.predict_on_pages(task=dummy_task, pages=[image])
     assert actual is not None
 
 
-def test_predict_task_ocr_w_image(
-    mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel
-):
+def test_predict_task_ocr_w_image(mock_minio, mock_model: PaddleInferOCR, dummy_task: TaskModel):
     dummy_task.input = InputForm(
         storage_file_path="tests/data/valid/cerfa_13750-05-1.pdf",
         raw_filename="cerfa_13750-05-1.pdf",
@@ -163,12 +139,10 @@ def test_predict_task_ocr_w_image(
         content_type="application/pdf",  # Uncomment this line to simulate the absence of content_type
     )
 
-    worker = OCRWorker(minio_connector=mock_minio, ocr_model=mock_model)
+    worker = OCRWorker(file_connector=mock_minio, ocr_model=mock_model)
     with open("tests/data/valid/cerfa_13750-05-1.pdf", "rb") as f:
         mock_minio.get_by_task_id.return_value = f
         task = worker.process_task_ocr(task=dummy_task)
         assert task.status == TaskStatus.COMPLETED.value
-        mock_minio.delete_by_task_id.assert_called_once_with(
-            user_id=dummy_task.user_id, task_id=dummy_task.id
-        )
+        mock_minio.delete_by_task_id.assert_called_once_with(user_id=dummy_task.user_id, task_id=dummy_task.id)
         assert task.output is not None
