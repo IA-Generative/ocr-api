b0171dffd3cb6ad644f06aaa1c7450311e4d521a
michou <misu.helal-ali@interieur.gouv.fr>
2025-05-16 16:08:38 +0200

    fix: cors config by default

--- ocr_backend/main.py
+++ ocr_backend/main.py
@@ -1,3 +1,5 @@
+import os
+import json
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -8,12 +10,18 @@ from src import __name__, __version__
 
 app = FastAPI(title=__name__, version=__version__)
 
+cors_config = {}
+path_cors_config = os.environ.get("CORS_CONFIG_PATH", "ocr_backend/config/cors.json")
+if os.path.exists(path_cors_config):
+    with open(path_cors_config) as config_file:
+        cors_config = json.load(config_file)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origin_regex=cors_config.get("allow_origin_regex", "*"),
+    allow_credentials=cors_config.get("allow_credentials", True),
+    allow_methods=cors_config.get("allow_methods", ["*"]),
+    allow_headers=cors_config.get("allow_headers", ["*"]),
 )
 
 app.include_router(task_router)
