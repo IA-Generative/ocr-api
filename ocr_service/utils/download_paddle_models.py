cedf48ff1535532944a663497ca2b440e50a80b5
michou <misu.helal-ali@interieur.gouv.fr>
2025-06-08 18:11:38 +0200

    feat: separate PaddleOCR and PPStructureV3 initialization into distinct files

--- ocr_service/utils/download_paddle_models.py
+++ ocr_service/utils/download_paddle_models.py
@@ -1,12 +1,6 @@
-from paddleocr import PaddleOCR, PPStructureV3
+from paddleocr import PaddleOCR
 
 # Initialize PaddleOCR instance
 PaddleOCR(
     use_doc_orientation_classify=False, use_doc_unwarping=False, use_textline_orientation=False, ocr_version="PP-OCRv5"
 )
-
-PaddleOCR(
-    use_doc_orientation_classify=False, use_doc_unwarping=False, use_textline_orientation=False, ocr_version="PP-OCRv5"
-)
-
-PPStructureV3()
