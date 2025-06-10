fc9f4478474dc98e1719fd761d9dd54a2043629f
michou <misu.helal-ali@interieur.gouv.fr>
2025-06-10 18:51:21 +0200

    fix: ensure directory check before file upload in test_upload_file

--- tests/ocr_backend/routers/test_jobs_route.py
+++ tests/ocr_backend/routers/test_jobs_route.py
@@ -1,4 +1,5 @@
 import glob
+from pathlib import Path
 import os
 import tempfile
 from uuid import uuid4
@@ -30,23 +31,24 @@ def test_upload_file(client, temp_file):
     user_id = str(uuid4())
 
     for img_path in glob.glob("tests/data/valid/*"):
-        with open(img_path, "rb") as image_file:
-            files = {"file": (img_path, image_file, "multipart/form-data")}
+        if not Path(img_path).is_dir:
+            with open(img_path, "rb") as image_file:
+                files = {"file": (img_path, image_file, "multipart/form-data")}
 
-            # Effectuer l'appel à l'API pour uploader le fichier
-            response = client.post(
-                f"/jobs/{user_id}",
-                files=files,
-            )
+                # Effectuer l'appel à l'API pour uploader le fichier
+                response = client.post(
+                    f"/jobs/{user_id}",
+                    files=files,
+                )
 
-        # Vérifier la réponse
-        assert response.status_code == 201
-        response_model = TaskModel(**response.json())
-        assert response_model.status == TaskStatus.QUEUED.value
-        assert response_model.user_id == user_id
+            # Vérifier la réponse
+            assert response.status_code == 201
+            response_model = TaskModel(**response.json())
+            assert response_model.status == TaskStatus.QUEUED.value
+            assert response_model.user_id == user_id
 
-        # Vérifier que le fichier est bien enregistré dans Minio
-        task_id = response_model.id
-        file_from_minio = s3_client_connector.get_by_task_id(user_id, task_id)
+            # Vérifier que le fichier est bien enregistré dans Minio
+            task_id = response_model.id
+            file_from_minio = s3_client_connector.get_by_task_id(user_id, task_id)
 
-        assert os.path.exists(file_from_minio)
+            assert os.path.exists(file_from_minio)
