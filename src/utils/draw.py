668ec43a0b5f33a81ac8d067552f8968f7577d9b
michou <misu.helal-ali@interieur.gouv.fr>
2025-06-10 10:35:49 +0200

    feat: implement draw_normalized_layout function for rendering layout boxes on images

--- src/utils/draw.py
+++ src/utils/draw.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 from src.schemas.box import Bbox
+from src.schemas.layout import Layout
 
 
 def draw_normalized_bboxes(
@@ -29,6 +30,48 @@ def draw_normalized_bboxes(
         y1 = (bb.y + bb.height) * img_h
         draw.rectangle([x0, y0, x1, y1], outline=outline, width=width)
         label = f"[{100*bb.confidence:.1f}]{bb.text}"
+        if font:
+            bbox = draw.textbbox((0, 0), label, font=font)
+            text_h = bbox[3] - bbox[1]
+        else:
+            # Estimation par défaut
+            text_h = 10
+
+        text_x = x0
+        text_y = max(0, y0 - text_h - 2)
+        draw.text((text_x, text_y), label, fill=outline, font=font)
+    return image
+
+
+def draw_normalized_layout(
+    image: Image.Image, layouts: list[Layout], outline="red", width=2, font: ImageFont.ImageFont = None
+):
+    """
+    Dessine les boîtes normalisées (x, y, w, h) sur l'image PIL.
+    :param image: PIL.Image original
+    :param boxes: liste d'objets avec .x, .y, .width, .height (normalisés)
+    :param outline: couleur de contour
+    :param width: épaisseur du trait en pixels
+    """
+    if font is None:
+        try:
+            # Police intégrée si dispo
+            font = ImageFont.load_default()
+        except Exception as e:
+            font = None
+            print(e)
+    draw = ImageDraw.Draw(image)
+    img_w, img_h = image.size
+    for bb in layouts:
+        # Convertit de coord normalisées vers pixels
+        x0, y0, x1, y1 = bb.coordinate
+        x0 = x0 * img_w
+        y0 = y0 * img_h
+        x1 = x1 * img_w
+        y1 = y1 * img_h
+
+        draw.rectangle([x0, y0, x1, y1], outline=outline, width=width)
+        label = f"[{100*bb.score:.1f}]{bb.label}"
         if font:
             bbox = draw.textbbox((0, 0), label, font=font)
             text_h = bbox[3] - bbox[1]
--- tests/ocr_service/models/test_layout_models.py
+++ tests/ocr_service/models/test_layout_models.py
@@ -1,25 +1,19 @@
 from PIL import Image
 from pathlib import Path
 from ocr_service.models.layout import PaddleLayoutDetection
+from src.utils.draw import draw_normalized_layout
 
 
 def test_predict_paddle_layout_detections():
     obj = PaddleLayoutDetection()
-    image = Image.open("tests/data/valid/formulaire-cerfa-complete.png")
+    path_image = Path("tests/data/valid/formulaire-cerfa-complete.png")
+    image = Image.open(path_image)
     actuals = obj.predict(images=[image, image])
 
     assert len(actuals) == 2
     folder_image = Path("tests/data/valid/formulaire-cerfa-complete/layouts")
     folder_image.mkdir(parents=True, exist_ok=True)
-    width_img, height_img = image.size
-    i = 0
-    for _, layouts in enumerate(actuals):
-        for j, layout in enumerate(layouts):
-            x1, y1, x2, y2 = layout.coordinate
-            x1 = int(x1 * width_img)
-            y1 = int(y1 * height_img)
-            x2 = int(x2 * width_img)
-            y2 = int(y2 * height_img)
-            box = (x1, y1, x2, y2)
-            cropped_image = image.crop(box)
-            cropped_image.save(folder_image / f"{i}-{j}-{layout.label}-formulaire-cerfa-complete.png")
+
+    for layouts in actuals:
+        image = draw_normalized_layout(image, layouts=layouts)
+        image.save(folder_image / f"{path_image.stem}{path_image.suffix}")
