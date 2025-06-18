faed3ba8d1a5153aa32d7ace4eb6cb2ab6889b7b
michou <misu.helal-ali@interieur.gouv.fr>
2025-06-18 12:17:29 +0200

    feat: Add BoxDetection model to worker and update Dockerfile and pyproject.toml

--- services/factory.py
+++ services/factory.py
@@ -1,6 +1,8 @@
 import boto3
 
 from services.base.worker import BaseWorker
+from business.checkbox_service.models.box_detection import BoxDetection
+
 from src.connector import S3Connector, s3_settings
 from src.logger import logger
 
@@ -10,6 +12,7 @@ s3_client_connector = S3Connector(s3_client=s3_client, bucket_name=s3_settings.S
 
 def load_worker(name: str, batch_size: int = 1, worker_weight: float = 1) -> BaseWorker:
     logger.info(f"---- {name} selected ----")
+    models = []
     model = None
     if name == "paddleocr-2.10.0":
         from business.paddleocr2.models.paddle import PaddleInferOCR2
@@ -22,17 +25,16 @@ def load_worker(name: str, batch_size: int = 1, worker_weight: float = 1) -> Bas
 
         model = PaddleInferOCR()
 
-    elif name == "checkbox":
-        from business.checkbox_service.models.box_detection import BoxDetection
-
-        model = BoxDetection()
     else:
         raise NotImplementedError("")
+
+    models.append(model)
+    models.append(BoxDetection())
 
     return BaseWorker(
         name=name,
         file_connector=s3_client_connector,
-        models=[model],
+        models=models,
         batch_size=batch_size,
         worker_weight=worker_weight,
     )
