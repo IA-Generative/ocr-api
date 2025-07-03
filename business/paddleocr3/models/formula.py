9ec7712af1a6f64e8e958d1e00ffb832dcff8d02
michou <misu.helal-ali@interieur.gouv.fr>
2025-07-03 16:30:29 +0200

    feat: Enhance logging in PaddleFormulaPrediction and PaddleLayoutDetection models for better traceability

--- business/paddleocr3/models/formula.py
+++ business/paddleocr3/models/formula.py
@@ -1,12 +1,17 @@
 from typing import List
+import logging
 from PIL import Image
 import numpy as np
+import time
 
 from src.schemas.layout import Layout
 from src.schemas.output import Page
 from services.base.model import BaseModelPrediction
 
 from paddleocr import FormulaRecognition
+from src.logger import logger
+
+logger.setLevel(logging.DEBUG)
 
 
 def crop_img(img: Image.Image, coordinates: list[float], is_normalized: bool = True):
@@ -29,7 +34,9 @@ class PaddleFormulaPredcition(BaseModelPrediction):
         device: str = "cpu",
         batch_size: int = 1,
     ):
+        logger.debug("FormulaRecognition start")
         self.model = FormulaRecognition(model_name=model_name, device=device)
+        logger.info("FormulaRecognition init")
         self.batch_size = batch_size
 
     def batch_predict(
@@ -55,26 +62,33 @@ class PaddleFormulaPredcition(BaseModelPrediction):
                 for i in range(len(images))
             ]
 
-        for page, image in zip(pages, images):
+        for j, (page, image) in enumerate(zip(pages, images)):
             layouts = page.layouts
+            logger.debug(f"[Formula] nb layouts {len(layouts)}")
             formula_layouts: list[Layout] = []
             formula_layouts_indices = []
             for i, layout in enumerate(layouts):
                 if layout.label == "formula":
                     formula_layouts_indices.append(i)
                     formula_layouts.append(layout)
+            logger.debug(f"[Formula] nb formula layouts {len(formula_layouts_indices)}")
             formula_crop_images: list[Image.Image] = []
             for formula_layout in formula_layouts:
                 crop_image = crop_img(image, coordinates=formula_layout.coordinate, is_normalized=True)
                 formula_crop_images.append(crop_image)
+            logger.debug(f"[Formula] nb images layouts {len(formula_crop_images)}")
+            t = time.time()
             result = self.model.predict(
                 [np.array(image.convert("RGB")) for image in formula_crop_images],
                 batch_size=self.batch_size,
             )
+            total_time = time.time() - t
+            logger.debug(f"[Formula] time to process {len(formula_crop_images)} - {total_time:.3f}s")
 
             for i, res in enumerate(result):
                 layouts[i].content = res["rec_formula"]
 
             page.layouts = layouts
+            pages[j] = page
 
         return pages
--- business/paddleocr3/models/layout.py
+++ business/paddleocr3/models/layout.py
@@ -1,5 +1,8 @@
 from typing import List, Union
+import logging
 from PIL import Image
+from time import time
+from uuid import uuid4
 
 from paddleocr import (
     LayoutDetection,
@@ -9,6 +12,9 @@ import numpy as np
 from src.schemas.layout import Layout
 from src.schemas.output import Page
 from services.base.model import BaseModelPrediction
+from src.logger import logger
+
+logger.setLevel(logging.DEBUG)
 
 
 class PaddleLayoutDetection(BaseModelPrediction):
@@ -18,7 +24,9 @@ class PaddleLayoutDetection(BaseModelPrediction):
         batch_size: int = 2,
         device: str = "cpu",
     ):
+        logger.debug("LayoutDection start")
         self.model = LayoutDetection(model_name=model_name, device=device, layout_nms=True)
+        logger.info("LayoutDection init")
         self.batch_size = batch_size
 
     def batch_predict(
@@ -32,11 +40,17 @@ class PaddleLayoutDetection(BaseModelPrediction):
             result = pages
         else:
             result = [Page(page=i, layouts=[]) for i in range(len(images))]
-
+        t = time()
         predictions = self.model.predict(
             [np.array(image.convert("RGB")) for image in images],
             batch_size=self.batch_size,
         )
+        process_time = time() - t
+        batch_process_id = uuid4().hex
+        logger.debug(
+            f"[Layout detection - {batch_process_id}]:  time to process {process_time:.3f}s nb image : {len(images)}"
+        )
+        layout_detected = {}
 
         for index_page, (image, pred) in enumerate(zip(images, predictions)):
             width_img, height_img = image.size
@@ -51,6 +65,10 @@ class PaddleLayoutDetection(BaseModelPrediction):
                 coordinates[3] = coordinates[3] / height_img
                 layouts[i].coordinate = coordinates
                 page_layouts.append(layouts[i])
+                if layouts[i].label not in layout_detected:
+                    layout_detected[layouts[i].label] = 0
+                layout_detected[layouts[i].label] += 1
+                logger.debug(f"[Layout detection - {batch_process_id}] layout detected {layout_detected}")
 
             result[index_page].layouts = page_layouts
 
