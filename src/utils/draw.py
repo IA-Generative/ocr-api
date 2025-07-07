f3e9df8dcd928d347f342d07aaa7a8b18871f6b5
michou <misu.helal-ali@interieur.gouv.fr>
2025-07-07 12:33:33 +0200

    feat: add checkbox drawing functionality and update tests with new images and JSON data

--- src/utils/draw.py
+++ src/utils/draw.py
@@ -1,10 +1,15 @@
 from PIL import Image, ImageDraw, ImageFont
 from src.schemas.box import Bbox
 from src.schemas.layout import Layout
+from src.schemas.box import Checkbox
 
 
 def draw_normalized_bboxes(
-    image: Image.Image, boxes: list[Bbox], outline="red", width=2, font: ImageFont.ImageFont = None
+    image: Image.Image,
+    boxes: list[Bbox],
+    outline="red",
+    width=2,
+    font: ImageFont.ImageFont = None,
 ):
     """
     Dessine les boîtes normalisées (x, y, w, h) sur l'image PIL.
@@ -44,7 +49,11 @@ def draw_normalized_bboxes(
 
 
 def draw_normalized_layout(
-    image: Image.Image, layouts: list[Layout], outline="red", width=2, font: ImageFont.ImageFont = None
+    image: Image.Image,
+    layouts: list[Layout],
+    outline="red",
+    width=2,
+    font: ImageFont.ImageFont = None,
 ):
     """
     Dessine les boîtes normalisées (x, y, w, h) sur l'image PIL.
@@ -72,6 +81,59 @@ def draw_normalized_layout(
 
         draw.rectangle([x0, y0, x1, y1], outline=outline, width=width)
         label = f"[{100*bb.score:.1f}]{bb.label}"
+        if font:
+            bbox = draw.textbbox((0, 0), label, font=font)
+            text_h = bbox[3] - bbox[1]
+        else:
+            # Estimation par défaut
+            text_h = 10
+
+        text_x = x0
+        text_y = max(0, y0 - text_h - 2)
+        draw.text((text_x, text_y), label, fill=outline, font=font)
+    return image
+
+
+def draw_normalized_checkboxes(
+    image: Image.Image,
+    checkboxes: list[Checkbox],
+    outline="red",
+    width=2,
+    font: ImageFont.ImageFont = None,
+):
+    """
+    Dessine les boîtes normalisées (x, y, w, h) sur l'image PIL.
+    :param image: PIL.Image original
+    :param boxes: liste d'objets avec .x, .y, .width, .height (normalisés)
+    :param outline: couleur de contour
+    :param width: épaisseur du trait en pixels
+    """
+    if font is None:
+        try:
+            # Police intégrée si dispo
+            font = ImageFont.load_default()
+        except Exception as e:
+            font = None
+            print(e)
+    draw = ImageDraw.Draw(image)
+    img_w, img_h = image.size
+    for bb in checkboxes:
+        # Convertit de coord normalisées vers pixels
+        (
+            x0,
+            y0,
+        ) = (
+            bb.x,
+            bb.y,
+        )
+        x1, y1 = x0 + bb.width, y0 + bb.height
+        x0 = x0 * img_w
+        y0 = y0 * img_h
+        x1 = x1 * img_w
+        y1 = y1 * img_h
+
+        draw.rectangle([x0, y0, x1, y1], outline=outline, width=width)
+        label = f"[{100*bb.confidence:.1f}]{ '[x]' if bb.confidence > 0.5 else '[ ]'}"
         if font:
             bbox = draw.textbbox((0, 0), label, font=font)
             text_h = bbox[3] - bbox[1]
--- tests/business/paddleocr3/models/test_pipeline_models.py
+++ tests/business/paddleocr3/models/test_pipeline_models.py
@@ -6,7 +6,11 @@ from business.paddleocr3.models.pipeline import PipelineLinearPrediction
 from business.paddleocr3.models.formula import PaddleFormulaRecognizer
 from business.paddleocr3.models.layout import PaddleLayoutDetection
 from business.paddleocr3.models.paddle import PaddleInferOCR
-from src.utils.draw import draw_normalized_layout, draw_normalized_bboxes
+from src.utils.draw import (
+    draw_normalized_layout,
+    draw_normalized_bboxes,
+    draw_normalized_checkboxes,
+)
 import json
 
 DEVICE = os.environ.get("DEVICE", "cpu")
@@ -46,6 +50,68 @@ def test_pipeline_ocr():
     folder_image = Path("tests/data/predictions/valid/Exo7-algebre-page40/bboxes")
     folder_image.mkdir(parents=True, exist_ok=True)
     result_image_bboxes = draw_normalized_bboxes(image, boxes=actual_pages[0].boxes)
+    w, h = result_image_bboxes.size
+    new_size = (w // 2, h // 2)
+
+    result_image_bboxes = result_image_bboxes.resize(new_size, Image.Resampling.LANCZOS)
+
+    result_image_bboxes.save(folder_image / f"{path_image.stem}{path_image.suffix}")
+
+
+def test_pipeline_ocr_checkbox():
+    from business.checkbox_service.models.box_detection import BoxDetection
+
+    obj = PipelineLinearPrediction(
+        models=[
+            PaddleInferOCR(device=DEVICE, lang="en", ocr_version="PP-OCRv4"),
+            PaddleLayoutDetection(device=DEVICE),
+            PaddleFormulaRecognizer(device=DEVICE),
+            BoxDetection(),
+        ]
+    )
+    path_image = Path("tests/data/valid/formulaire-cerfa-complete.png")
+    image = Image.open(path_image).convert("RGB")
+
+    actual_pages = obj.batch_predict(images=[image])
+    assert len(actual_pages) == 1
+    assert isinstance(actual_pages[0], Page)
+    assert len(actual_pages[0].layouts) != 0
+    for layout in actual_pages[0].layouts:
+        print(layout.label, layout.content)
+
+    ####### Layouts #######
+    folder_image = Path("tests/data/predictions/valid/formulaire-cerfa-complete/layouts")
+    folder_image.mkdir(parents=True, exist_ok=True)
+
+    result_image = draw_normalized_layout(image=image.copy(), width=5, layouts=actual_pages[0].layouts)
+
+    w, h = result_image.size
+    new_size = (w // 2, h // 2)
+
+    result_image = result_image.resize(new_size, Image.Resampling.LANCZOS)
+
+    result_image.save(folder_image / f"{path_image.stem}{path_image.suffix}")
+    with open(folder_image / f"{path_image.stem}.json", "w") as f:
+        json.dump([lay.model_dump() for lay in actual_pages[0].layouts], f, indent=2)
+
+    ####### Layouts #######
+    ####### BBoxes  #######
+
+    folder_image = Path("tests/data/predictions/valid/formulaire-cerfa-complete/bboxes")
+    folder_image.mkdir(parents=True, exist_ok=True)
+    result_image_bboxes = draw_normalized_bboxes(image.copy(), boxes=actual_pages[0].boxes)
+    w, h = result_image_bboxes.size
+    new_size = (w // 2, h // 2)
+
+    result_image_bboxes = result_image_bboxes.resize(new_size, Image.Resampling.LANCZOS)
+
+    result_image_bboxes.save(folder_image / f"{path_image.stem}{path_image.suffix}")
+    ####### BBoxes  #######
+
+    ####### Checkboxes  #######
+    folder_image = Path("tests/data/predictions/valid/formulaire-cerfa-complete/checkboxes")
+    folder_image.mkdir(parents=True, exist_ok=True)
+    result_image_bboxes = draw_normalized_checkboxes(image.copy(), checkboxes=actual_pages[0].checkboxes)
     w, h = result_image_bboxes.size
     new_size = (w // 2, h // 2)
 
