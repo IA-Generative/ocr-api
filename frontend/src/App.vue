22ff899613bcf657dec6cd8b30ed270bddb1dd57
mitchou10 <helal.michou.junk@gmail.com>
2025-07-17 17:31:25 +0200

    Preprod (#95)
    * Staging (#92)
    
    * feat: @kevindb frontend (#89)
    
    * feat: @kevindb frontend
    
    * ci: add ocr frontend
    
    ---------
    
    Co-authored-by: michou <misu.helal-ali@interieur.gouv.fr>
    
    * Dev (#90)
    
    * ci: fix flower image
    
    * test: fix test security token
    
    * fix: :bug: fix nginx (#91)
    
    * fix: dockerfile frontend
    
    ---------
    
    Co-authored-by: michou <misu.helal-ali@interieur.gouv.fr>
    Co-authored-by: Kevin De Benedetti <debenedetti.k@gmail.com>
    
    ---------
    
    Co-authored-by: michou <misu.helal-ali@interieur.gouv.fr>
    Co-authored-by: Kevin De Benedetti <debenedetti.k@gmail.com>
    
    * fix: front personalization (#94)
    
    * feat: @kevindb frontend (#89)
    
    * feat: @kevindb frontend
    
    * ci: add ocr frontend
    
    ---------
    
    Co-authored-by: michou <misu.helal-ali@interieur.gouv.fr>
    
    * ci: fix flower image
    
    * test: fix test security token
    
    * fix: :bug: fix nginx (#91)
    
    * chore: :sparkles: update titles & seo & header & pages (root)
    
    * Staging (#92)
    
    * feat: @kevindb frontend (#89)
    
    * feat: @kevindb frontend
    
    * ci: add ocr frontend
    
    ---------
    
    Co-authored-by: michou <misu.helal-ali@interieur.gouv.fr>
    
    * Dev (#90)
    
    * ci: fix flower image
    
    * test: fix test security token
    
    * fix: :bug: fix nginx (#91)
    
    * fix: dockerfile frontend
    
    ---------
    
    Co-authored-by: michou <misu.helal-ali@interieur.gouv.fr>
    Co-authored-by: Kevin De Benedetti <debenedetti.k@gmail.com>
    
    ---------
    
    Co-authored-by: michou <misu.helal-ali@interieur.gouv.fr>
    Co-authored-by: Kevin De Benedetti <debenedetti.k@gmail.com>
    
    ---------
    
    Co-authored-by: mitchou10 <helal.michou.junk@gmail.com>
    Co-authored-by: michou <misu.helal-ali@interieur.gouv.fr>
    
    ---------
    
    Co-authored-by: michou <misu.helal-ali@interieur.gouv.fr>
    Co-authored-by: Kevin De Benedetti <debenedetti.k@gmail.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,22 +9,6 @@ const serviceTitle = 'OCR'
 const serviceDescription = 'Reconnaître un texte scanné'
 const logoText = ['Ministère', 'de l’intérieur']
 
-const quickLinks = [
-  {
-    label: 'Home',
-    to: '/',
-    icon: 'ri-home-4-line',
-    iconAttrs: { color: 'var(--red-marianne-425-625)' },
-  },
-  {
-    label: 'À propos',
-    to: '/a-propos',
-    icon: 'ri-question-mark',
-    iconRight: true,
-  },
-]
-const searchQuery = ref('')
-
 const {
   offlineReady,
   needRefresh,
@@ -39,12 +23,9 @@ function close () {
 
 <template>
   <DsfrHeader
-    v-model="searchQuery"
     :service-title="serviceTitle"
     :service-description="serviceDescription"
     :logo-text="logoText"
-    :quick-links="quickLinks"
-    show-search
   />
 
   <div class="fr-container  fr-mt-3w  fr-mt-md-5w  fr-mb-5w">
