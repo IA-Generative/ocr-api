b4a4dd0a949ef4d5743b1300df0d8fb1daad247f
mitchou10 <helal.michou.junk@gmail.com>
2025-07-17 11:56:42 +0200

    Dev (#86) (#87)
    * clean: clean repo
    
    * feat: add auth by token (#85)
    
    
    
    ---------
    
    Co-authored-by: michou <misu.helal-ali@interieur.gouv.fr>

---
+++ ocr_backend/core/security/factory.py
@@ -0,0 +1,37 @@
+import os
+import warnings
+from ocr_backend.core.security.token import BaseVerifyToken, RequestContext
+
+
+class AllowAllAccess(BaseVerifyToken):
+    def __init__(self, verify_token=True):
+        super().__init__(verify_token, is_fastapi=True)
+        warnings.warn(message="YOU USE DEV MODE PLEASE DON'T USE THAT IN PRODUCTION")
+
+    def verify(self, ctx: RequestContext) -> bool:
+        return True
+
+
+class DevToken(BaseVerifyToken):
+    def __init__(self):
+        super().__init__(verify_token=True, is_fastapi=True)
+        warnings.warn(message="YOU USE DEV MODE PLEASE DON'T USE THAT IN PRODUCTION")
+        self.user_info = {
+            "token1": RequestContext(user_id="test1", email="r@exemple.com", roles=[], token="token1"),
+            "token2": RequestContext(user_id="test2", email="r@exemple.com", roles=[], token="token2"),
+        }
+
+    def verify(self, ctx: RequestContext):
+        if ctx.token in self.user_info:
+            current_user = self.user_info[ctx.token]
+            return ctx.user_id == current_user.user_id
+
+        return False
+
+
+SECURITY_FACTORY: dict[str, BaseVerifyToken] = {
+    "full-access": AllowAllAccess(),
+    "dev": DevToken(),
+}
+
+TokenVerifier: BaseVerifyToken = SECURITY_FACTORY[os.environ.get("VERIFY_TOEKN_MODEL", "full-access")]
