87fa2e5888a726913cc3d0ad630a93114fe8e69a
michou <misu.helal-ali@interieur.gouv.fr>
2025-07-26 14:40:07 +0200

    refacto: checkbox

--- business/llm/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-
-class OpenAISetting(BaseSettings):
-    OPENAI_API_KEY: str
-    OPENAI_BASE_URL: str
-    INSTRUCT_MODEL_NAME: str
-    VISION_MODEL: str = None
