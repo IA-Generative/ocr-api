2c0f39203206b5cd90ab35866d2836f90f6680a8
michou <misu.helal-ali@interieur.gouv.fr>
2025-07-26 14:25:33 +0200

    refacto: clean repo

--- ocr_backend/connectors.py
+++
@@ -1,6 +0,0 @@
-import boto3
-
-from src.connector import S3Connector, s3_settings
-
-s3_client = boto3.client("s3")
-s3_client_connector = S3Connector(s3_client=s3_client, bucket_name=s3_settings.S3_BUCKET_NAME)
