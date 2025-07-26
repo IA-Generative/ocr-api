f65ca9cc60350b4c815c3d18252fde5f495f63a7
michou <misu.helal-ali@interieur.gouv.fr>
2025-07-26 14:26:09 +0200

    del: delete src folder

--- src/config/__init__.py
+++
@@ -1,6 +0,0 @@
-from .celery import CelerySettings
-from .ocr_model import OCRModelSettings
-from .redis import RedisSettings
-from .s3 import S3Settings
-
-__all__ = ["S3Settings", "RedisSettings", "OCRModelSettings", "CelerySettings"]
--- src/schemas/__init__.py
+++
@@ -1,8 +0,0 @@
-from src.connector import Base, engine
-
-from .task import Task
-from .task_stats import TaskStatTable
-
-__all__ = ["Task", "TaskStatTable", "Base", "engine"]
-
-# Base.metadata.create_all(engine)
