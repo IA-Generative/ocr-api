5ac7ef0a3c9abbbbcd8de0d31b7e13e9b19e1bad
michou <misu.helal-ali@interieur.gouv.fr>
2025-07-29 10:24:59 +0200

    chore: update pre-commit hooks and dependencies; add forms directory to Dockerfile; enhance integration tests
    feat: add workflow for testing services related to forms

--- apps/server/tests/integrations/business/paddle2/test_factory_integration.py
+++ apps/server/tests/integrations/business/paddle2/test_factory_integration.py
@@ -2,6 +2,7 @@ import os
 import pytest
 
 from src.schemas.task import TaskForm, TaskModel, task_table, TaskStatus
+from src.schemas.output import FormEntry
 from src.schemas.input import InputForm
 from src.logger import logger
 from services.factory import load_worker, s3_client_connector
@@ -17,7 +18,7 @@ def test_load_worker_paddleocr2_integration(dummy_task: TaskModel):
     dummy_task.input = InputForm(
         type="file",
         storage_file_path="tests/data/valid/cerfa_13750-05-1.pdf",
-        raw_filename="cerfa_13750-05-1.pdf",
+        raw_filename="tests/data/valid/cerfa_13750-05-1.pdf",
         content_type="application/pdf",
         ext=".pdf",
         size=123456,
@@ -34,3 +35,31 @@ def test_load_worker_paddleocr2_integration(dummy_task: TaskModel):
     task = worker.process_task(task=dummy_task)
     assert task.percentage == 1
     assert task.status == TaskStatus.COMPLETED.value
+
+
+@pytest.mark.skipif(os.environ["PROCESS_NAME"] != "paddleocr-2.10.0", reason="Is not paddleocr-2.10.0")
+def test_load_worker_paddleocr2_with_form_integration(dummy_task: TaskModel):
+    dummy_task.input = InputForm(
+        type="file",
+        storage_file_path="tests/data/valid/cerfa_11573-09-filled.pdf",
+        raw_filename="tests/data/valid/cerfa_11573-09-filled.pdf",
+        content_type="application/pdf",
+        ext=".pdf",
+        size=123456,
+    )
+    saved_path = s3_client_connector.save(
+        user_id=dummy_task.user_id,
+        task_id=dummy_task.id,
+        file_path="tests/data/valid/cerfa_11573-09-filled.pdf",
+    )
+    dummy_task.input.storage_file_path = saved_path
+    logger.info(saved_path)
+    logger.info(79 * "*")
+    worker = load_worker(name="paddleocr-2.10.0")
+    task = worker.process_task(task=dummy_task)
+    assert task.percentage == 1
+    assert task.status == TaskStatus.COMPLETED.value
+    assert len(task.output.pages[1].form_entries) > 0
+    assert len(task.output.pages[1].boxes) > 0
+
+    assert isinstance(task.output.pages[1].form_entries[0], FormEntry)
