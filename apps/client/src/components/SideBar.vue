52f324628dfcc03084345c44af0296596e3a03ea
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-08-12 12:42:51 +0200

    fix: correct attribut href to in DsfrTile component

--- apps/client/src/components/SideBar.vue
+++ apps/client/src/components/SideBar.vue
@@ -88,7 +88,7 @@ const donneMonAvisButton = {
         <DsfrTile
           :key="index"
           :title="tool.title"
-          :href="PORTAIL_URL + tool.to"
+          :to="PORTAIL_URL + tool.to"
           :icon="false"
           horizontal
           :img-src="tool.imgSrc"
