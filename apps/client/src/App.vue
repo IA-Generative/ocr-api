bdd164815e826d5c899452c0279a4c58ae1884b9
Alexandre Morin <alexandre.morin@ext.bpifrance.fr>
2026-02-16 17:35:02 +0100

    :speech_balloon: rename dropdown label

--- apps/client/src/App.vue
+++ apps/client/src/App.vue
@@ -1,42 +1,11 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { getKeycloak } from '@/utils/keycloak'
 import useToaster from './composables/use-toaster'
 
-const keycloak = getKeycloak()
-const isLoggedIn = computed(() => keycloak.authenticated)
-
 const toaster = useToaster()
-
-const serviceTitle = 'Reconnaître un texte scanné'
-const serviceDescription = 'Extraire le texte d\'une image ou d\'un document scanné'
-const logoText = ['Ministère', 'de l’intérieur']
-
-const quickLinks = computed(() => {
-  const items = []
-  if (!isLoggedIn.value) {
-    items.push(
-      { label: 'Se connecter', to: '/login', class: 'fr-icon-user-fill' },
-    )
-  }
-  else {
-    items.push(
-      { label: 'Se déconnecter', to: '/logout', class: 'fr-icon-logout-box-r-line' }
-    )
-  }
-
-  return items
-})
 </script>
 
 <template>
-  <DsfrHeader
-    :service-title="serviceTitle"
-    :service-description="serviceDescription"
-    :logo-text="logoText"
-    :quick-links="quickLinks"
-  />
-
+  <AppHeader />
   <div class="fr-container  fr-mt-3w  fr-mt-md-5w  fr-mb-5w">
     <router-view />
   </div>
--- apps/client/src/components.d.ts
+++ apps/client/src/components.d.ts
@@ -12,11 +12,14 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    AppHeader: typeof import('./components/AppHeader.vue')['default']
     AppToaster: typeof import('./components/AppToaster.vue')['default']
     CustomCard: typeof import('./components/CustomCard.vue')['default']
     DsfrAlert: typeof import('@gouvminint/vue-dsfr')['DsfrAlert']
     DsfrButton: typeof import('@gouvminint/vue-dsfr')['DsfrButton']
+    DsfrButtonGroup: typeof import('@gouvminint/vue-dsfr')['DsfrButtonGroup']
     DsfrCallout: typeof import('@gouvminint/vue-dsfr')['DsfrCallout']
+    DsfrDropdown: typeof import('./components/DsfrDropdown.vue')['default']
     DsfrFileUpload: typeof import('@gouvminint/vue-dsfr')['DsfrFileUpload']
     DsfrHeader: typeof import('@gouvminint/vue-dsfr')['DsfrHeader']
     DsfrPagination: typeof import('@gouvminint/vue-dsfr')['DsfrPagination']
@@ -32,11 +35,14 @@ declare module 'vue' {
 
 // For TSX support
 declare global {
+  const AppHeader: typeof import('./components/AppHeader.vue')['default']
   const AppToaster: typeof import('./components/AppToaster.vue')['default']
   const CustomCard: typeof import('./components/CustomCard.vue')['default']
   const DsfrAlert: typeof import('@gouvminint/vue-dsfr')['DsfrAlert']
   const DsfrButton: typeof import('@gouvminint/vue-dsfr')['DsfrButton']
+  const DsfrButtonGroup: typeof import('@gouvminint/vue-dsfr')['DsfrButtonGroup']
   const DsfrCallout: typeof import('@gouvminint/vue-dsfr')['DsfrCallout']
+  const DsfrDropdown: typeof import('./components/DsfrDropdown.vue')['default']
   const DsfrFileUpload: typeof import('@gouvminint/vue-dsfr')['DsfrFileUpload']
   const DsfrHeader: typeof import('@gouvminint/vue-dsfr')['DsfrHeader']
   const DsfrPagination: typeof import('@gouvminint/vue-dsfr')['DsfrPagination']
