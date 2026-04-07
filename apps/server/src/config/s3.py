33d197f8fa8590a8de6c53fb1a8b68809e8bb7d0
mitchou10 <helal.michou.junk@gmail.com>
2026-04-07 18:37:30 +0200

    fix: enable SSL verification for S3 client and improve code formatting (#254)

--- apps/server/src/config/s3.py
+++ apps/server/src/config/s3.py
@@ -3,4 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class S3Settings(BaseSettings):
     S3_BUCKET_NAME: str = "test"
-    model_config = SettingsConfigDict(from_attributes=True, case_sensitive=True, env_file=".env", extra="allow")
+    VERIFY_SSL: bool = False
+    model_config = SettingsConfigDict(
+        from_attributes=True, case_sensitive=True, env_file=".env", extra="allow"
+    )
